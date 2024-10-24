8cea6d5a77e1ce2232e4008fb5396db25a66ab8e
SahilBas2005 <sahilbas2005@gmail.com>
2024-10-24 03:51:54 -0400

    Keeps track of current user when logging in

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Sets the current user.
+     * @param name The username
+     */
+    void setCurrentUser(String name);
 }
